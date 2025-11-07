367f3829593f67a56309d6dafcff34bd391fab40
alani25 <alanihamzah25@gmail.com>
2025-11-07 15:33:35 -0500

    setup + created class list

---
+++ MicroTools/src/components/ListGroup.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+
+interface Props {
+    items: string[];
+    name: string;
+}
+
+function ListGroup({ items, name }: Props) {
+
+
+
+    // // event handler
+    // const handleClick = (event: MouseEvent) => console.log(event);
+
+    // selected index
+    const [selectedIndex, setSelectedIndex] = useState(0);
+
+
+    return (
+        <>
+            <h1>{name}</h1>
+            <ul className="list-group">
+
+                {items.map((item, index) => (
+                    <li
+                        className={selectedIndex == index ? "list-group-item active" : "list-group-item"}
+                        key={item}
+                        onClick={() => setSelectedIndex(index)}>
+                        {item}
+                    </li>
+                ))}
+
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
+
